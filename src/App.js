129ceb08f3d97ab31d90b222f38fe46e38084a29
Sofie <anna.sofie.ohlsson@gmail.com>
2023-02-28 18:43:23 +0000

    routinggg

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter, Routes, Route} from "react-router-dom"; 
+import { HashRouter ,Routes, Route} from "react-router-dom"; 
 
 import './App.css';
 
@@ -13,7 +13,7 @@ import PageNotFound from './pages/PageNotFound';
 
 function App() {
   return (
-    <BrowserRouter> 
+<HashRouter>
       <Navbar />
       <Routes>
         <Route path="cv/" element={<Home />} />
@@ -23,7 +23,7 @@ function App() {
         <Route path="*" element={<PageNotFound />} />
       </Routes>
       <Footer />
-    </BrowserRouter>
+      </HashRouter>
   );
 }
 
