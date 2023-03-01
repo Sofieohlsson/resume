df7597bb2992b03626c59027d89df6293428bb83
Sofie <anna.sofie.ohlsson@gmail.com>
2023-03-01 10:39:18 +0000

    resume testing

--- src/components/navbar/NavItem.js
+++ src/components/navbar/NavItem.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function NavItem({ name }) {
     return(
-        <a href={`${name}`}>
+        <a href={`/resume/${name}`}>
             <div>{name}</div>
         </a>
     )
