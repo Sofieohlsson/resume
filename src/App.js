1fa6b98b8e2762d77a15e8f1b83f3c1f1ce7576d
Sofie <anna.sofie.ohlsson@gmail.com>
2023-03-01 15:13:58 +0000

    router in index

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter ,Routes, Route} from "react-router-dom"; 
+import { Routes, Route} from "react-router-dom"; 
 
 import './App.css';
 
@@ -13,7 +13,7 @@ import PageNotFound from './pages/PageNotFound';
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -23,7 +23,7 @@ function App() {
         <Route path="*" element={<PageNotFound />} />
       </Routes>
       <Footer />
-    </BrowserRouter>
+    </>
   );
 }
 
